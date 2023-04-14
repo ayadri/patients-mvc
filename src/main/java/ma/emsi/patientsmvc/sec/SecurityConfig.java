5ed42791e5954c1d33fd906eb9cbe2d215981412
Aya <driouichayaa@gmail.com>
2023-04-14 13:34:09 +0000

    first commit

---
+++ src/main/java/ma/emsi/patientsmvc/sec/SecurityConfig.java
@@ -0,0 +1,9 @@
+package ma.emsi.patientsmvc.sec;
+
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
